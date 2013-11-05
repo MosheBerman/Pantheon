1aa3ad16f1ab1e3baa3c44bed9c749faef687d82
Moshe <moshberm@gmail.com>
2013-11-05 11:41:10 -0500

    Renameed PBXProj to be PBXProjDotPBXProject.

---
+++ Pantheon/Model/Xcode Project/PBXProjectDotPBXProj.h
@@ -0,0 +1,71 @@
+//
+//  PNProject.h
+//  Pantheon
+//
+//  Created by Moshe Berman on 10/15/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PBXBuildFile.h"
+#import "PBXFileReference.h"
+
+@class PBXReferenceProxy;
+@class PBXFrameworksBuildPhase;
+@class PBXProject_Obj;
+@class PBXResourcesBuildPhase;
+@class PBXSourcesBuildPhase;
+@class PBXTargetDependency;
+@class PBXVariantGroup;
+@class XCBuildConfiguration;
+
+@interface PBXProjectDotPBXProj : NSObject
+
+/** The archive version. */
+@property (nonatomic, strong) NSNumber *archiveVersion;
+
+/** A dictionary of classes. */
+@property (nonatomic, strong) NSDictionary *classes;
+
+/**
+ *  The object version.
+ */
+@property (nonatomic, strong) NSNumber *objectVersion;
+
+/** Root Object ID. */
+@property (nonatomic, strong) NSString *rootObjectID;
+
+/** The URL that the project resides at. */
+@property (nonatomic, strong) NSURL *projectURL;
+
+/* The different parts of the objects group... */
+@property (nonatomic, strong) NSMutableArray *PBXBuildFiles;
+@property (nonatomic, strong) NSMutableArray *PBXFrameworksBuildPhases;
+@property (nonatomic, strong) PBXReferenceProxy *PBXReferenceProxy;
+@property (nonatomic, strong) NSMutableArray *PBXFileReferences;
+@property (nonatomic, strong) NSMutableArray *PBXGroups;
+@property (nonatomic, strong) NSMutableArray *PBXNativeTargets;
+@property (nonatomic, strong) PBXProject_Obj *PBXProject;
+@property (nonatomic, strong) NSMutableArray *PBXResourcesBuildPhases;
+@property (nonatomic, strong) NSMutableArray *PBXScriptBuildPhases;
+@property (nonatomic, strong) NSMutableArray *PBXSourcesBuildPhases;
+@property (nonatomic, strong) NSMutableArray *PBXTargetDependencies;
+@property (nonatomic, strong) NSMutableArray *PBXVariantGroups;
+@property (nonatomic, strong) NSMutableArray *XCBuildConfigurations;
+
+@property (nonatomic, strong) NSMutableArray *PBXContainerItemProxies;
+@property (nonatomic, strong) NSDictionary *XCVersionGroup;
+
+/**
+ *  Loads an Xcode project out from a given url.
+ *
+ *  @param url The url to an xcodeproj or pbxproj file.
+ *  @param error An error object to pass to the initializer.
+ *
+ *  @return A configured Xcode project.
+ */
+
++ (id)projectFromXcodeProjectAtURL:(NSURL *)url error:(NSError **)error;
+
+@end
---
+++ Pantheon/Model/Xcode Project/PBXProjectDotPBXProj.m
@@ -0,0 +1,102 @@
+//
+//  PNProject.m
+//  Pantheon
+//
+//  Created by Moshe Berman on 10/15/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "PBXProjectDotPBXProj.h"
+
+@interface PBXProjectDotPBXProj ()
+
+@end
+
+@implementation PBXProjectDotPBXProj
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _PBXBuildFiles = [[NSMutableArray alloc] init];
+        _PBXContainerItemProxies = [[NSMutableArray alloc] init];
+        _PBXFileReferences = [[NSMutableArray alloc] init];
+        _PBXFrameworksBuildPhases = [[NSMutableArray alloc] init];
+        _PBXGroups = [[NSMutableArray alloc] init];
+        _PBXNativeTargets = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
++ (id)projectFromXcodeProjectAtURL:(NSURL *)url error:(NSError **)error
+{
+    
+    PBXProjectDotPBXProj *project = [[PBXProjectDotPBXProj alloc] init];
+    
+    if (project) {
+        
+        /* If the URL is an Xcode project, we want to look for the pbxproj file. */
+        
+        if ([[url pathExtension] isEqualToString:@"xcodeproj"]) {
+            
+            /* Save the original project URL before we manipulate it. */
+            [project setProjectURL:url];
+            
+            url = [url URLByAppendingPathComponent:@"project.pbxproj"];
+        }
+        
+        /* Now, we either have a valid project.pbxproj file or not. */
+        if ([[url lastPathComponent] isEqualToString:@"project.pbxproj"])
+        {
+            
+            NSError *err = nil;
+            
+            NSDictionary *projectContents = [NSDictionary dictionaryWithContentsOfFile:[url path]];
+            
+            /* */
+            if (!projectContents) {
+                project = nil;
+                *error = err;
+            }
+            
+            /*  If we've had a successful load, populate the object. */
+            else {
+                
+                [project setArchiveVersion:projectContents[@"archiveVersion"]];
+                
+                [project setClasses:projectContents[@"classes"]];
+                
+                [project setObjectVersion:projectContents[@"objectVersion"]];
+                
+                [project setRootObjectID:projectContents[@"rootObject"]];
+                
+                /* Load the objects... */
+                
+                NSDictionary *objectsInProject = projectContents[@"objects"];
+                
+                for (NSString *key in [objectsInProject allKeys]) {
+                    
+                    /* Get the object itself. */
+                    NSDictionary *object = objectsInProject[key];
+                
+                    /* Based on the kind of object, do the right thing with it. */
+                    
+                    /* If we've got a file reference, add it to the project. */
+                    if ([object[@"isa"] isEqualToString:@"PBXFileReference"]) {
+                        [project.PBXFileReferences addObject:[PBXFileReference fileReferenceWithIdentifier:key andDictionary:object]];
+                    }
+                }
+            }
+        }
+    }
+    
+    /* Else, we don't have a valid Xcode project, so let's just quit. */
+    else {
+        project = nil;
+    }
+    
+    return project;
+}
+
+
+@end
