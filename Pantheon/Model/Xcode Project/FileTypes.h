5f36268a777120643a3d739617afb30a20718057
Moshe <moshberm@gmail.com>
2013-11-14 03:27:33 -0500

    Added a file types header.

---
+++ Pantheon/Model/Xcode Project/FileTypes.h
@@ -0,0 +1,225 @@
+//
+//  FileTypes.h
+//  Pantheon
+//
+//  Created by Moshe Berman on 11/14/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#ifndef Pantheon_FileTypes_h
+#define Pantheon_FileTypes_h
+
+/**
+ *  This file contains strings that represent the
+ *  kinds of files that Pantheon can handle.
+ */
+
+NSString *FileTypeNone = nil;
+
+NSString *FileTypearchiveAr = @"archive.ar";
+
+
+NSString *FileTypeArchiveAsdictionary = @"archive.asdictionary";
+
+NSString *FileTypeArchiveBinhex = @"archive.binhex";
+
+NSString *FileTypeArchiveEar = @"archive.ear";
+
+NSString *FileTypeArchiveGzip = @"archive.gzip";
+
+NSString *FileTypeArchiveJar = @"archive.jar";
+
+NSString *FileTypeArchiveMacbinary = @"archive.macbinary";
+
+NSString *FileTypeArchivePpob = @"archive.ppob";
+
+NSString *FileTypeArchiveRsrc = @"archive.rsrc";
+
+NSString *FileTypeArchiveStuffit = @"archive.stuffit";
+
+NSString *FileTypeArchiveTar = @"archive.tar";
+
+NSString *FileTypeArchiveWar = @"archive.war";
+
+NSString *FileTypeArchiveZip = @"archive.zip";
+
+NSString *FileTypeAudioAiff = @"audio.aiff";
+
+NSString *FileTypeAudioAu = @"audio.au";
+
+NSString *FileTypeAudioMidi = @"audio.midi";
+
+NSString *FileTypeAudioMp3 = @"audio.mp3";
+
+NSString *FileTypeAudioWav = @"audio.wav";
+
+NSString *FileTypeCompiledMachO = @"compiled.mach-o";
+
+NSString *FileTypeCompiledMachOBundle = @"compiled.mach-o.bundle";
+
+NSString *FileTypeCompiledMachOCorefile = @"compiled.mach-o.corefile";
+
+NSString *FileTypeCompiledMachODylib = @"compiled.mach-o.dylib";
+
+NSString *FileTypeCompiledMachOFvmlib = @"compiled.mach-o.fvmlib";
+
+NSString *FileTypeCompiledMachOObjfile = @"compiled.mach-o.objfile";
+
+NSString *FileTypeCompiledMachOPreload = @"compiled.mach-o.preload";
+
+NSString *FileTypeFileBplist = @"file.bplist";
+
+NSString *FileTypeFileXib = @"file.xib";
+
+NSString *FileTypeImageBmp = @"image.bmp";
+
+NSString *FileTypeImageGif = @"image.gif";
+
+NSString *FileTypeImageIcns = @"image.icns";
+
+NSString *FileTypeImageIco = @"image.ico";
+
+NSString *FileTypeImageJpeg = @"image.jpeg";
+
+NSString *FileTypeImagePdf = @"image.pdf";
+
+NSString *FileTypeImagePict = @"image.pict";
+
+NSString *FileTypeImagePng = @"image.png";
+
+NSString *FileTypeImageTiff = @"image.tiff";
+
+NSString *FileTypePatternProxy = @"pattern.proxy";
+
+NSString *FileTypeSourcecodeAda = @"sourcecode.ada";
+
+NSString *FileTypeSourcecodeApplescript = @"sourcecode.applescript";
+
+NSString *FileTypeSourcecodeAsm = @"sourcecode.asm";
+
+NSString *FileTypeSourcecodeAsmAsm = @"sourcecode.asm.asm";
+
+NSString *FileTypeSourcecodeAsmLlvm = @"sourcecode.asm.llvm";
+
+NSString *FileTypeSourcecodeC = @"sourcecode.c";
+
+NSString *FileTypeSourcecodeCCPreprocessed = @"sourcecode.c.c.preprocessed";
+
+NSString *FileTypeSourcecodeCH = @"sourcecode.c.h";
+
+NSString *FileTypeSourcecodeCObjc = @"sourcecode.c.objc";
+
+NSString *FileTypeSourcecodeCObjcPreprocessed = @"sourcecode.c.objc.preprocessed";
+
+NSString *FileTypeSourcecodeCppCpp = @"sourcecode.cpp.cpp";
+
+NSString *FileTypeSourcecodeCppCppPreprocessed = @"sourcecode.cpp.cpp.preprocessed";
+
+NSString *FileTypeSourcecodeCppH = @"sourcecode.cpp.h";
+
+NSString *FileTypeSourcecodeCppObjcpp = @"sourcecode.cpp.objcpp";
+
+NSString *FileTypeSourcecodeCppObjcppPreprocessed = @"sourcecode.cpp.objcpp.preprocessed";
+
+NSString *FileTypeSourcecodeDtrace = @"sourcecode.dtrace";
+
+NSString *FileTypeSourcecodeExports = @"sourcecode.exports";
+
+NSString *FileTypeSourcecodeFortran = @"sourcecode.fortran";
+
+NSString *FileTypeSourcecodeFortranF77 = @"sourcecode.fortran.f77";
+
+NSString *FileTypeSourcecodeFortranF90 = @"sourcecode.fortran.f90";
+
+NSString *FileTypeSourcecodeGlsl = @"sourcecode.glsl";
+
+NSString *FileTypeSourcecodeJam = @"sourcecode.jam";
+
+NSString *FileTypeSourcecodeJava = @"sourcecode.java";
+
+NSString *FileTypeSourcecodeJavascript = @"sourcecode.javascript";
+
+NSString *FileTypeSourcecodeLex = @"sourcecode.lex";
+
+NSString *FileTypeSourcecodeMake = @"sourcecode.make";
+
+NSString *FileTypeSourcecodeMig = @"sourcecode.mig";
+
+NSString *FileTypeSourcecodeNasm = @"sourcecode.nasm";
+
+NSString *FileTypeSourcecodeOpencl = @"sourcecode.opencl";
+
+NSString *FileTypeSourcecodePascal = @"sourcecode.pascal";
+
+NSString *FileTypeSourcecodeRez = @"sourcecode.rez";
+
+NSString *FileTypeSourcecodeYacc = @"sourcecode.yacc";
+
+NSString *FileTypeText = @"text";
+
+NSString *FileTypeTextCss = @"text.css";
+
+NSString *FileTypeTextHtmlDocumentation = @"text.html.documentation";
+
+NSString *FileTypeTextMan = @"text.man";
+
+NSString *FileTypeTextPbxproject = @"text.pbxproject";
+
+NSString *FileTypeTextPlist = @"text.plist";
+
+NSString *FileTypeTextPlistInfo = @"text.plist.info";
+
+NSString *FileTypeTextPlistScriptSuite = @"text.plist.scriptSuite";
+
+NSString *FileTypeTextPlistScriptTerminology = @"text.plist.scriptTerminology";
+
+NSString *FileTypeTextPlistStrings = @"text.plist.strings";
+
+NSString *FileTypeTextPlistXclangspec = @"text.plist.xclangspec";
+
+NSString *FileTypeTextPlistXcsynspec = @"text.plist.xcsynspec";
+
+NSString *FileTypeTextPlistXctxtmacro = @"text.plist.xctxtmacro";
+
+NSString *FileTypeTextPlistXml = @"text.plist.xml";
+
+NSString *FileTypeTextRtf = @"text.rtf";
+
+NSString *FileTypeTextScript = @"text.script";
+
+NSString *FileTypeTextScriptCsh = @"text.script.csh";
+
+NSString *FileTypeTextScriptPerl = @"text.script.perl";
+
+NSString *FileTypeTextScriptPhp = @"text.script.php";
+
+NSString *FileTypeTextScriptPython = @"text.script.python";
+
+NSString *FileTypeTextScriptRuby = @"textScript.ruby";
+
+NSString *FileTypeTextScriptSh = @"text.scriptSh";
+
+NSString *FileTypeTextScriptWorksheet = @"text.script.worksheet";
+
+NSString *FileTypeTextXcconfig = @"text.xcconfig";
+
+NSString *FileTypeTextXml = @"text.xml";
+
+NSString *FileTypeVideoAvi = @"video.avi";
+
+NSString *FileTypeVideoMpeg = @"video.mpeg";
+
+NSString *FileTypeVideoQuartzComposer = @"video.quartz-composer";
+
+NSString *FileTypeVideoQuicktime = @"video.quicktime";
+
+NSString *FileTypeWrapperApplication = @"wrapper.application";
+
+NSString *FileTypeWrapperCFBundle = @"wrapper.cfbundle";
+
+NSString *FileTypeWrapperFramework = @"wrapper.framework";
+
+NSString *FileTypeWrapperPBProject = @"wrapper.pb-project";
+
+
+#endif
